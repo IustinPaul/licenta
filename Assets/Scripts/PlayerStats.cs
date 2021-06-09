14bc47ebe5ae2e842ad5d93cdec9f39993614acf
IustinPaul <iustinbuhuta@live.com>
2021-06-09 17:04:18 +0300

    UI player

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerStats : MonoBehaviour
 {
@@ -12,40 +13,50 @@ public class PlayerStats : MonoBehaviour
     [SerializeField] private float m_staminaRegenPerSec = 5.0f;
     [SerializeField] private float m_rollStaminaCost = 20.0f;
     [SerializeField] private float m_attackStaminaCost = 10.0f;
+    [SerializeField] private Color m_highLife;
+    [SerializeField] private Color m_mediumLife;
+    [SerializeField] private Color m_lowLife;
 
     private Animator m_animator;
+    private RectTransform m_lifeBar;
+    private RectTransform m_staminaBar;
+    private Image m_lifeBarImage;
+    private float m_maxSizeLifeBar;
+    private float m_maxSizeStaminaBar;
     private float m_currentLife = 100.0f;
     private float m_currentStamina = 100.0f;
     private float m_armor = 1.0f;
     private float m_timeSinceDmg;
-    private float m_nrSeconds = 0.0f;
     void Awake()
     {
         m_animator = GetComponent<Animator>();
+        m_lifeBar = transform.GetChild(1).GetChild(0).GetChild(0) as RectTransform;
+        m_staminaBar = transform.GetChild(1).GetChild(1).GetChild(0) as RectTransform;
+        m_lifeBarImage = m_lifeBar.GetComponent<Image>();
+        m_maxSizeLifeBar = m_lifeBar.rect.width;
+        m_maxSizeStaminaBar = m_staminaBar.rect.width;
         m_timeSinceDmg = m_delayDmgTaken;
     }
 
     void Update()
     {
+        float dt = Time.deltaTime;
         m_timeSinceDmg += Time.deltaTime;
-        m_nrSeconds += Time.deltaTime;
 
-        if(m_nrSeconds > 1.0f)
+        m_currentLife += m_lifeRegenPerSec * dt;
+        if(m_currentLife > m_totalLife)
         {
-            m_nrSeconds -= 1.0f;
-
-            m_currentLife += m_lifeRegenPerSec;
-            if(m_currentLife > m_totalLife)
-            {
-                m_currentLife = m_totalLife;
-            }
-
-            m_currentStamina += m_staminaRegenPerSec;
-            if(m_currentStamina > m_totalStamina)
-            {
-                m_currentStamina = m_totalStamina;
-            }
+            m_currentLife = m_totalLife;
         }
+
+        m_currentStamina += m_staminaRegenPerSec * dt;
+        if(m_currentStamina > m_totalStamina)
+        {
+            m_currentStamina = m_totalStamina;
+        }
+
+        UpdateLifeBar();
+        UpdateStaminaBar();
     }
 
     public void TakeDmg(float dmg)
@@ -53,6 +64,7 @@ public class PlayerStats : MonoBehaviour
         if(m_timeSinceDmg > m_delayDmgTaken)
         {
             m_currentLife -= dmg / m_armor;
+            UpdateLifeBar();
             m_timeSinceDmg = 0;
             if(m_currentLife <= 0)
             {
@@ -73,6 +85,7 @@ public class PlayerStats : MonoBehaviour
         if (m_currentStamina >= m_rollStaminaCost)
         {
             m_currentStamina -= m_rollStaminaCost;
+            UpdateStaminaBar();
             return true;
         }
 
@@ -85,9 +98,34 @@ public class PlayerStats : MonoBehaviour
         if (m_currentStamina >= m_attackStaminaCost)
         {
             m_currentStamina -= m_attackStaminaCost;
+            UpdateStaminaBar();
             return true;
         }
 
         return false;
+    }
+
+    private void UpdateLifeBar()
+    {
+        m_lifeBar.sizeDelta= new Vector2(m_maxSizeLifeBar / m_totalLife * m_currentLife, m_lifeBar.rect.height);
+        if(m_maxSizeLifeBar/2 < m_lifeBar.rect.width)
+        {
+            m_lifeBarImage.color = m_highLife;
+        }
+        else if(m_maxSizeLifeBar / 4 < m_lifeBar.rect.width)
+        {
+
+            m_lifeBarImage.color = m_mediumLife;
+        }
+        else
+        {
+
+            m_lifeBarImage.color = m_lowLife;
+        }
+    }
+
+    private void UpdateStaminaBar()
+    {
+        m_staminaBar.sizeDelta = new Vector2(m_maxSizeStaminaBar / m_totalStamina * m_currentStamina, m_staminaBar.rect.height);
     }
 }
